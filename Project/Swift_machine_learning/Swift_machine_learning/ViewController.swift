8cd177a52e67bb3eef6ae0bd981d0a56e87437f3
shinseunghyun <paigeshin1991gmail.com>
2020-04-11 15:15:49 +0900

    first commit

---
+++ Project/Swift_machine_learning/Swift_machine_learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_machine_learning
+//
+//  Created by shin seunghyun on 2020/04/11.
+//  Copyright © 2020 shin seunghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
